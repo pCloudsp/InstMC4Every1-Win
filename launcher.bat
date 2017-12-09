4617f2b812ca15b5f57f436ec06c2a8d53f5d78c
pCloudsp <zzr233666@outlook.com>
2017-12-09 10:38:41 +0800

    undone

--- launcher.bat
+++ launcher.bat
@@ -1,30 +1,58 @@
 @echo off
 cls
+cd %~dp0
 
-echo ===========================================================
-echo Minecraft原版服务端一键安装脚本Windows版 主脚本 v5.0_alpha1
-echo -----------------------作者: +Clouds+----------------------
-echo ---------------博客: https://tec4every1.com ---------------
-echo =================正在做一些初始化操作......================
+echo =============================================================
+echo Minecraft原版服务端一键安装脚本Windows版 启动脚本 v5.0_alpha1
+echo -----------------------作者: +Clouds+------------------------
+echo ---------------博客: https://tec4every1.com -----------------
+echo =================正在做一些初始化操作......==================
 
 :initialize
-echo '在Windows系统里什么包管理器都是不存在的，跳过...'
-echo '检测必要依赖安装情况...'
-
-echo %path%|find "java" >nul
-if %errorlevel% equ 0 (
-	echo 检测到Java。继续...
-) else (
-	echo 未检测到Java。正在安装...
+if exist %homepath%\xmcalreadyran (
+	echo '在Windows系统里什么包管理器都是不存在的，跳过...'
+	echo '检测必要依赖安装情况...'
+	::CheckJava
+	echo %path%|find "java" >nul
+	if %errorlevel% equ 0 (
+		echo 检测到Java。继续...
+	) else (
+		echo 未检测到Java。正在安装...
+		echo %processor_architecture% | find "64" >nul
+		if %errorlevel% equ 0 (
+			./jre8u152x64.exe /s
+			echo 64位的Java正在后台被安装。你可以继续配置进程。
+		) else (
+			./jre8u152x86.exe /s
+			echo 32位的Java正在后台被安装。你可以继续配置进程。
+		)
+	)
+	::InstallGit
+	echo %path%|find "java" >nul
+	if %errorlevel% equ 0 (
+		echo 正在安装64位的git。
+		./git64.exe /silent
+	) else (
+		echo 正在安装32位的git。
+		./git32.exe /silent
+	)
+	::InstallWget
+	echo 正在安装Wget。
+	./wgetsetup.exe /s
 	echo %processor_architecture% | find "64" >nul
 	if %errorlevel% equ 0 (
-		echo 你的系统是64位的。正在下载对应的安装包...
-		bitsadmin.exe /transfer "Downloading Java installation binary" http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jre-8u152-windows-x64.exe %homepath%\Downloads\jre-installer.exe
-		%homepath%\Downloads\jre-installer.exe /s
-		echo Java正在下载并安装。你可以在这段时间内同时使用本脚本配置服务器。
+		set PATH=%PATH%;%PROGRAMFILES(X86)%\GnuWin32\bin
 	) else (
-		echo 你的系统是32位的。正在下载对应的安装包...
-		bitsadmin.exe /transfer "Downloading Java installation binary" http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jre-8u152-windows-i586.exe %homepath%\Downloads\jre-installer.exe
-		%homepath%\Downloads\jre-installer.exe /s
-		echo Java正在下载并安装。你可以在这段时间内同时使用本脚本配置服务器。
+		set PATH=%PATH%;%PROGRAMFILES%\GnuWin32\bin
 	)
+	echo 初始化完毕！
+	exit
+) else (
+	if %errorlevel% equ 0 (
+		set PATH=%PATH%;%PROGRAMFILES(X86)%\GnuWin32\bin
+	) else (
+		set PATH=%PATH%;%PROGRAMFILES%\GnuWin32\bin
+	)
+	echo 初始化完毕！
+	./main.bat
+)
